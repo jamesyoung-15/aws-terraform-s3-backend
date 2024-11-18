282d3d91fa0bbebcf955cc0ee9e032ff9c6ae9c2
James <jyyoung@jyymail.com>
2024-11-18 10:32:36 -0500

    quick updates

--- example-backend.tf
+++ example-backend.tf
@@ -1,9 +1,11 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-backend-jyylab"
+    bucket = "my-bucket" # Change to bucket name
     key    = "testing/test-backend"
     region = "us-east-1"
-    dynamodb_table = "terraform-state-lock"
+    dynamodb_table = "terraform-state-locks" # Change to DynamoDB table name
     encrypt = true
+    shared_config_files = [ "~/.aws/credentials" ] # point to AWS credentials file
+    profile = "my_account" # set profile name where backend bucket is located
   }
 }
