3362633f628b7e86b08cf73c55adca374fe7522a
James <jyyoung@jyymail.com>
2024-11-23 01:45:03 -0500

    update example

--- example/example-backend.tf
+++ example/example-backend.tf
@@ -1,11 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "my-bucket" # Change to bucket name
+    bucket = "terraform-state-backend-jyylab" # Change to bucket name
     key    = "testing/test-backend"
     region = "us-east-1"
-    dynamodb_table = "terraform-state-locks" # Change to DynamoDB table name
+    dynamodb_table = "terraform-state-lock-jyylab" # Change to DynamoDB table name
     encrypt = true
-    shared_config_files = [ "~/.aws/credentials" ] # point to AWS credentials file
-    profile = "my_account" # set profile name where backend bucket is located
   }
 }
--- example/main.tf
+++ example/main.tf
@@ -1,15 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-backend-jyylab" # Change to bucket name
-    key    = "testing/test-backend"
-    region = "us-east-1"
-    dynamodb_table = "terraform-state-locks" # Change to DynamoDB table name
-    encrypt = true
-    shared_config_files = [ "~/.aws/credentials" ] # point to AWS credentials file
-    profile = "Deployments" # set profile name where backend bucket is located
-  }
-}
-
 resource "random_string" "bucket_suffix" {
   length = 7
   special = false
