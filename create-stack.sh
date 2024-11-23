ccc0ac3f0c34eedbe478f0d17a6173fca9e796f3
James <jyyoung@jyymail.com>
2024-11-23 01:17:15 -0500

    update repo

--- create-stack.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-aws cloudformation create-stack --template-body file://terraform-state-s3-backend.yml --stack-name Terraform-State-Backend-Resources --region us-east-1 --profile Deployments
--- validate-stack.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Validate the stack
-aws cloudformation validate-template --template-body file://terraform-state-s3-backend.yml --region=us-east-1 --profile=Deployments
