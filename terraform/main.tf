843f22498263c8603b323a7ffa1ceab9b4eb7151
James <jyyoung@jyymail.com>
2024-11-23 01:27:21 -0500

    fix format and add principal to policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 # set region
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 # get root organization id
@@ -9,6 +9,10 @@ data "aws_organizations_organization" "caller_org" {}
 # allow access to s3 bucket only from the organization
 data "aws_iam_policy_document" "s3_policy" {
   statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
     effect    = "Allow"
     actions   = ["s3:GetObject", "s3:PutObject"]
     resources = [aws_s3_bucket.s3_state_bucket.arn]
@@ -23,6 +27,10 @@ data "aws_iam_policy_document" "s3_policy" {
 # allow access to dynamodb table only from the organization
 data "aws_iam_policy_document" "dynamodb_policy" {
   statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
     effect    = "Allow"
     actions   = ["dynamodb:DescribeTable", "dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:DeleteItem"]
     resources = []
@@ -59,7 +67,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
   bucket = aws_s3_bucket.s3_state_bucket.id
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
+      sse_algorithm = "AES256"
     }
   }
 }
@@ -78,7 +86,7 @@ resource "aws_s3_bucket_policy" "s3_allow_access_from_org" {
   bucket = aws_s3_bucket.s3_state_bucket.id
   policy = data.aws_iam_policy_document.s3_policy.json
 
-  depends_on = [ aws_s3_bucket_public_access_block.s3_block_public_access ]
+  depends_on = [aws_s3_bucket_public_access_block.s3_block_public_access]
 }
 
 
@@ -88,7 +96,7 @@ resource "aws_dynamodb_table" "lock_table" {
   name         = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
-  
+
   attribute {
     name = "LockID"
     type = "S"
@@ -107,5 +115,5 @@ resource "aws_dynamodb_table" "lock_table" {
 # attach policy to dynamodb table
 resource "aws_dynamodb_resource_policy" "lock_table_policy" {
   resource_arn = aws_dynamodb_table.lock_table.arn
-  policy = data.aws_iam_policy_document.dynamodb_policy.json
+  policy       = data.aws_iam_policy_document.dynamodb_policy.json
 }
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,9 +1,9 @@
 output "dynamodb_table_name" {
   description = "DynamicDB table for locking."
-  value = aws_dynamodb_table.lock_table
+  value       = aws_dynamodb_table.lock_table
 }
 
 output "bucket_name" {
   description = "S3 bucket backend for state."
-  value = aws_s3_bucket.s3_state_bucket
+  value       = aws_s3_bucket.s3_state_bucket
 }
