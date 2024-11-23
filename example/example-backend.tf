21e137dd89719d9219ff7d0fcc53d70e4f734b10
James <jyyoung@jyymail.com>
2024-11-23 15:39:21 -0500

    update example

--- example/example-backend.tf
+++ example/example-backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "terraform-state-backend-jyylab" # Change to bucket name
     key    = "testing/test-backend"
     region = "us-east-1"
-    dynamodb_table = "terraform-state-lock-jyylab" # Change to DynamoDB table name
+    dynamodb_table = "terraform-state-lock-jyylab" # Change to DynamoDB table name, might need to use ARN
     encrypt = true
   }
 }
